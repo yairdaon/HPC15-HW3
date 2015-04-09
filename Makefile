ef1ae6406f60ad75d87f29a81e0a286bb6e37c01
Yair Daon <yair.daon@gmail.com>
2015-04-09 16:58:14 -0400

    solved some

---
+++ Makefile
@@ -0,0 +1,48 @@
+#COMPILER = gcc -fopenmp
+#EXECUTABLES = omp_solved2 omp_solved3 omp_solved4 omp_solved5 omp_solved6
+
+solved2: omp_solved2.c
+	gcc  -fopenmp omp_solved2.c -o solved2
+
+2: solved2
+	./solved2
+
+
+
+solved3: omp_solved3.c
+	gcc -fopenmp omp_solved3.c -o solved3
+
+3: solved3
+	./solved3
+
+
+
+
+
+solved4: omp_solved4.c
+	gcc -fopenmp omp_solved4.c -o solved4
+
+4: solved4
+	./solved4
+
+
+
+omp_solved5: omp_solved5.c
+	gcc -fopenmp omp_solved5.c -o solved5
+
+5: solved5
+	./solved5
+
+
+
+solved6: solved6.c
+	gcc -fopenmp omp_solved6.c -o solved6
+
+6: solved
+	./solved6
+
+
+
+clean:
+	rm *~
+	rm -rvf solved?
--- omp_solved2.c
+++ omp_solved2.c
@@ -11,30 +11,38 @@
 
 int main (int argc, char *argv[]) 
 {
-int nthreads, i, tid;
-float total;
+  int nthreads, i, tid;
+  int n = 50;
+  float total;
 
-/*** Spawn parallel region ***/
+  /*** Spawn parallel region ***/
 #pragma omp parallel 
   {
-  /* Obtain thread number */
-  tid = omp_get_thread_num();
-  /* Only master thread does this */
-  if (tid == 0) {
-    nthreads = omp_get_num_threads();
-    printf("Number of threads = %d\n", nthreads);
+    /* Obtain thread number */
+    tid = omp_get_thread_num();
+    /* Only master thread does this */
+    if (tid == 0) {
+      nthreads = omp_get_num_threads();
+      printf("Number of threads = %d\n", nthreads);
     }
-  printf("Thread %d is starting...\n",tid);
+    printf("Thread %d is starting...\n",tid);
 
-  #pragma omp barrier
+#pragma omp barrier
+  
+    /* do some work */
+    total = 0.0;
+#pragma omp parallel for reduction(+:total)
+    for (i=0; i < n; ++i) {
+      total +=  i*1.0;
+    }
 
-  /* do some work */
-  total = 0.0;
-  #pragma omp for schedule(dynamic,10)
-  for (i=0; i<1000000; i++) 
-     total = total + i*1.0;
+#pragma omp barrier
+  printf ("done! Total = %e\n",total);  
+  printf ("Should be   = %e\n",  (n-1.0)*n/2.0       );
+  
 
-  printf ("Thread %d is done! Total= %e\n",tid,total);
+  } /*** end parallel ***/
 
-  } /*** End of parallel region ***/
 }
+// added a reduce command and added barrier before print statement
+// for some reason i can't get the thread ids right...
